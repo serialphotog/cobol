c58e3b736a2afbd58407ae69145eec0e4cd8d6a7
Adam Thompson <adam@serialphotog.com>
2021-02-10 15:23:59 -0500

    Added a hello world

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Various COBOL programs I've written for learning purposes.
+
+# Contents
+
+* hello.cbl - A simple hello world implemenation
---
+++ hello.cbl
@@ -0,0 +1,7 @@
+      * A simple hello world implementation
+		IDENTIFICATION DIVISION.
+		PROGRAM-ID. HelloWorld.
+		AUTHOR. Adam Thompson.
+		PROCEDURE DIVISION.
+		DISPLAY "Hello, World!".
+		STOP RUN.
